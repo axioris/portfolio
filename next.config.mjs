e70f32df54f67bfaf3cec1a51e66a706c0f54652
axioris <dane.foster.collins@gmail.com>
2025-01-16 08:12:09 -0800

    personal portfolio: update config

--- next.config.mjs
+++ next.config.mjs
@@ -1,13 +1,12 @@
 /** @type {import('next').NextConfig} */
-const isProd = true;
 
 const nextConfig = {
   reactStrictMode: true,
   images: {
     unoptimized: true, // Disable default image optimization
   },
-  basePath: isProd ? '/portfolio' : '',
-  assetPrefix: isProd ? '/portfolio/' : '',
+  basePath: '/portfolio',
+  assetPrefix: '/portfolio',
   output: 'export',
 };
 
