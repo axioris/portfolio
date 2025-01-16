c9d46a6b919b51dfad048b920d6102263f915603
axioris <dane.foster.collins@gmail.com>
2025-01-16 11:14:26 -0800

    personal portfolio: work properly on local development

--- next.config.mjs
+++ next.config.mjs
@@ -3,12 +3,9 @@ const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
   reactStrictMode: true,
-  images: {
-    unoptimized: true, // Disable default image optimization
-  },
-  basePath: isProd ? '/portfolio' : '',
-  assetPrefix: isProd ? '/portfolio' : '',
-  output: 'export',
+  // basePath: isProd ? '/portfolio' : '',
+  // assetPrefix: isProd ? '/portfolio' : '',
+  // output: 'export',
 };
 
 export default nextConfig;
